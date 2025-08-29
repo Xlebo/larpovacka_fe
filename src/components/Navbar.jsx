b9508973fb28a05b97cd7278dac1f10e5894fa14
Xlebo <samo.chlebo@gmail.com>
2025-08-29 16:25:55 +0200

    logout button fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,23 +16,25 @@ export default function Navbar({user, setUser}) {
     }
 
     function rightCorner() {
-        if (user == null) {
-            return (
-                <>
-                    <ul>
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/pravidla">Pravidlá</NavLink></li>
-                        <li><NavLink to="/prakticke">Praktické</NavLink></li>
-                        <li><NavLink to="/contacts">Org Tím</NavLink></li>
-                        <li><NavLink to="/faq">FAQ</NavLink></li>
-                        <li className="login"><NavLink to="/login">Login</NavLink></li>
-                        <li className="register"><NavLink to="/register">Registrácia</NavLink></li>
-                    </ul>
-                </>
-            )
-        }
         return (
-            <li className="right-li"><a onClick={logout}>Log out</a></li>
+            <>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/pravidla">Pravidlá</NavLink></li>
+                    <li><NavLink to="/prakticke">Praktické</NavLink></li>
+                    <li><NavLink to="/contacts">Org Tím</NavLink></li>
+                    <li><NavLink to="/faq">FAQ</NavLink></li>
+                    {(user == null) ?
+                        <>
+                            <li className="login"><NavLink to="/login">Login</NavLink></li>
+                            <li className="register"><NavLink to="/register">Registrácia</NavLink></li>
+                        </>
+                        :
+                        // classname used only for style, timed bombs ahead :D
+                        <li className="login" onClick={logout}><a style={{ cursor: "pointer" }}>Log out</a></li>
+                    }
+                </ul>
+            </>
         )
     }
 
